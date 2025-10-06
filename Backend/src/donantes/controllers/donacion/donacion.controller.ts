a6bf91c3f7b168f3c02b5c3e8a13f7c96c549d99
Jordyfr2004 <jordyalex32@gmail.com>
2025-10-05 20:13:56 -0500

    implementacion-controladores en la parte donantes

--- Backend/src/donantes/controllers/donacion/donacion.controller.ts
+++ Backend/src/donantes/controllers/donacion/donacion.controller.ts
@@ -1,9 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CreateDonacionDto } from 'src/donantes/domain/dto/create-donacion.dto/create-donacion.dto';
+import { DonacionService } from 'src/donantes/services/donacion/donacion.service';
 
 @Controller('donacion')
 export class DonacionController {
+    constructor(private readonly donacionService: DonacionService ) {}
     @Get()
-    getinit(): string{
-        return "Hola desde donacion";
+    findAll() {
+        return this.donacionService.finAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id:string) {
+        return this.donacionService.findById(id);
+    }
+
+    @Post()
+    create(@Body() dto: CreateDonacionDto) {
+        return this.donacionService.create(dto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id:string){
+        return this.donacionService.remove(id);
     }
 }
+
+
--- Backend/src/donantes/controllers/donador/donantes.controller.ts
+++ Backend/src/donantes/controllers/donador/donantes.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CreateDonadorDto } from 'src/donantes/domain/dto/create-donador.dto/create-donador.dto';
+import { DonadorService } from 'src/donantes/services/donador/donador.service';
 
 @Controller('donantes')
-export class DonantesController {}
+export class DonantesController {
+    constructor(private readonly donantesService: DonadorService) {}
+
+    @Get()
+    findAll() {
+        return this.donantesService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.donantesService.findById(id);
+    }
+
+    @Post()
+    create(@Body() dto: CreateDonadorDto){
+        return this.donantesService.create(dto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.donantesService.remove(id);
+    }
+}
